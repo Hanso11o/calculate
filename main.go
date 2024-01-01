d93f5e9a75de4f9391555910031eb7790242031f
denis erofeev <deniserofeev84@gmail.com>
2024-01-01 12:49:59 +0300

    fix errors

--- main.go
+++ main.go
@@ -8,219 +8,141 @@ import (
 	"strings"
 )
 
-// Создадим функцию main, в которой будет осуществляться ввод данных и вызов функций для их обработки:
+const (
+	LOW = "Вывод ошибки, так как строка " +
+		"не является математической операцией."
+	HIGH = "Вывод ошибки, так как формат математической операции " +
+		"не удовлетворяет заданию — два операнда и один оператор (+, -, /, *)."
+	SCALE = "Вывод ошибки, так как используются " +
+		"одновременно разные системы счисления."
+	DIV = "Вывод ошибки, так как в римской системе " +
+		"нет отрицательных чисел."
+	ZERO  = "Вывод ошибки, так как в римской системе нет числа 0."
+	RANGE = "Калькулятор умеет работать только с арабскими целыми " +
+		"числами или римскими цифрами от 1 до 10 включительно"
+)
+
+var roman = map[string]int{
+	"X":    10,
+	"IX":   9,
+	"VIII": 8,
+	"VII":  7,
+	"VI":   6,
+	"V":    5,
+	"IV":   4,
+	"III":  3,
+	"II":   2,
+	"I":    1,
+}
+
+var convIntToRoman = [10]int{
+	10,
+	9,
+	8,
+	7,
+	6,
+	5,
+	4,
+	3,
+	2,
+	1,
+}
+
+var a, b *int
+
+var operators = map[string]func() int{
+	"+": func() int { return *a + *b },
+	"-": func() int { return *a - *b },
+	"/": func() int { return *a / *b },
+	"*": func() int { return *a * *b },
+}
+
+var data []string
+
+func base(s string) {
+	var operator string
+	var stringsFound int
+	numbers := make([]int, 0)
+	romans := make([]string, 0)
+	romansToInt := make([]int, 0)
+	for idx := range operators {
+		for _, val := range s {
+			if idx == string(val) {
+				operator += idx
+				data = strings.Split(s, operator)
+			}
+		}
+	}
+	switch {
+	case len(operator) > 1:
+		panic(HIGH)
+	case len(operator) < 1:
+		panic(LOW)
+	}
+	for _, elem := range data {
+		num, err := strconv.Atoi(elem)
+		if err != nil {
+			stringsFound++
+			romans = append(romans, elem)
+		} else {
+			numbers = append(numbers, num)
+		}
+	}
+
+	switch stringsFound {
+	case 1:
+		panic(SCALE)
+	case 0:
+		errCheck := numbers[0] > 0 && numbers[0] < 11 &&
+			numbers[1] > 0 && numbers[1] < 11
+		if val, ok := operators[operator]; ok && errCheck == true {
+			a, b = &numbers[0], &numbers[1]
+			fmt.Println(val())
+		} else {
+			panic(RANGE)
+		}
+	case 2:
+		for _, elem := range romans {
+			if val, ok := roman[elem]; ok && val > 0 && val < 11 {
+				romansToInt = append(romansToInt, val)
+			} else {
+				panic(RANGE)
+			}
+		}
+		if val, ok := operators[operator]; ok {
+			a, b = &romansToInt[0], &romansToInt[1]
+			intToRoman(val())
+		}
+	}
+}
+func intToRoman(romanResult int) {
+	var romanNum string
+	if romanResult == 0 {
+		panic(ZERO)
+	} else if romanResult < 0 {
+		panic(DIV)
+	}
+	for romanResult > 0 {
+		for _, elem := range convIntToRoman {
+			for i := elem; i <= romanResult; {
+				for index, value := range roman {
+					if value == elem {
+						romanNum += index
+						romanResult -= elem
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(romanNum)
+}
 
 func main() {
-
+	fmt.Println("Введите выражение от 0 до 10 например, 2 + 6 или II + VI")
 	reader := bufio.NewReader(os.Stdin)
-
-	fmt.Print("Введите выражение например, 2 + 2 или II + II ")
-
-	expression, _ := reader.ReadString('\n')
-
-	expression = strings.TrimSpace(expression)
-
-	result, err := evaluateExpression(expression)
-
-	if err != nil {
-
-		fmt.Println("Ошибка:", err)
-
-	} else {
-
-		fmt.Println("Результат:", result)
-
+	for {
+		console, _ := reader.ReadString('\n')
+		s := strings.ReplaceAll(console, " ", "")
+		base(strings.ToUpper(strings.TrimSpace(s)))
 	}
-
 }
-
-// Внутри функции main считываем выражение с помощью функции ReadString из пакета bufio.
-// Мы также используем функции TrimSpace для удаления лишних пробелов и других символов пунктуации.
-// Затем вызываем функцию evaluateExpression, которая будет обрабатывать введенное выражение и возвращать результат.
-// Теперь создадим функцию evaluateExpression для обработки выражения:
-
-func evaluateExpression(expression string) (int, error) {
-
-	isArabic := isArabicExpression(expression)
-
-	if isArabic {
-
-		return evaluateArabicExpression(expression)
-
-	}
-
-	return evaluateRomanExpression(expression)
-
-}
-
-// Эта функция сначала вызывает функцию isArabicExpression, которая определяет, является ли выражение арабским числом или римским.
-// В зависимости от результата, функция вызывает соответствующую функцию для обработки выражения: evaluateArabicExpression для арабских чисел и evaluateRomanExpression для римских чисел.
-// Реализуем функцию isArabicExpression, которая будет проверять, содержит ли выражение только арабские числа:
-
-func isArabicExpression(expression string) bool {
-
-	_, err := strconv.Atoi(expression)
-
-	return err == nil
-
-}
-
-// Мы используем функцию Atoi из пакета strconv, чтобы попытаться преобразовать выражение в число.
-// Если преобразование прошло успешно, то это арабское число.
-// Теперь создадим функцию evaluateArabicExpression для обработки арабского выражения:
-
-func evaluateArabicExpression(expression string) (int, error) {
-
-	parts := strings.Split(expression, " ")
-
-	if len(parts) != 3 {
-
-		return 0, fmt.Errorf("некорректное выражение")
-
-	}
-
-	firstOperand, err := strconv.Atoi(parts[0])
-
-	if err != nil {
-
-		return 0, fmt.Errorf("некорректное выражение")
-
-	}
-
-	secondOperand, err := strconv.Atoi(parts[2])
-
-	if err != nil {
-
-		return 0, fmt.Errorf("некорректное выражение")
-
-	}
-
-	switch operator := parts[1]; operator {
-
-	case "+":
-
-		return firstOperand + secondOperand, nil
-
-	case "-":
-
-		return firstOperand - secondOperand, nil
-
-	case "*":
-
-		return firstOperand * secondOperand, nil
-
-	case "/":
-
-		return firstOperand / secondOperand, nil
-
-	default:
-
-		return 0, fmt.Errorf("неизвестный оператор")
-
-	}
-
-}
-
-func evaluateRomanExpression(expression string) (int, error) {
-
-	parts := strings.Split(expression, " ")
-
-	if len(parts) != 3 {
-
-		return 0, fmt.Errorf("некорректное выражение")
-
-	}
-
-	firstOperand, err := romanToArabic(parts[0])
-
-	if err != nil {
-
-		return 0, fmt.Errorf("некорректное выражение")
-
-	}
-
-	// В этой функции мы сначала разбиваем выражение на три части, используя пробел в качестве разделителя, и проверяем, что количество частей равно трем.
-	// Если нет, то возвращаем ошибку.
-	// Затем преобразуем первый и второй операнды в числа, используя функцию Atoi. Если преобразование не удалось, то возвращаем ошибку.
-	// Затем проверяем оператор с помощью конструкции switch и выполняем соответствующую операцию.
-	// Наконец, создадим функцию evaluateRomanExpression для обработки римского выражения:
-
-	secondOperand, err := romanToArabic(parts[2])
-
-	if err != nil {
-
-		return 0, fmt.Errorf("некорректное выражение")
-
-	}
-
-	switch operator := parts[1]; operator {
-
-	case "+":
-
-		return firstOperand + secondOperand, nil
-
-	case "-":
-
-		return firstOperand - secondOperand, nil
-
-	case "*":
-
-		return firstOperand * secondOperand, nil
-
-	case "/":
-
-		return firstOperand / secondOperand, nil
-
-	default:
-
-		return 0, fmt.Errorf("неизвестный оператор")
-
-	}
-
-}
-
-// В этой функции мы используем функцию romanToArabic, которая преобразует римское число в арабское.
-// Затем мы выполняем аналогичные действия для операндов и оператора, что и в функции evaluateArabicExpression.
-// Наконец, реализуем функцию romanToArabic:
-
-func romanToArabic(roman string) (int, error) {
-
-	romanNumerals := map[rune]int{
-
-		'V': 5,
-		'I': 1,
-		'X': 10,
-	}
-	// TO DO...
-	//Разобраться или найти библеотеку для работы с римскими числами..
-
-	var result int
-
-	for i := 0; i < len(roman); i++ {
-
-		if i > 0 && romanNumerals[rune(roman[i])] > romanNumerals[rune(roman[i-1])] {
-
-			result += romanNumerals[rune(roman[i])] - 2*romanNumerals[rune(roman[i-1])]
-
-		} else {
-
-			result += romanNumerals[rune(roman[i])]
-
-		}
-
-	}
-
-	if romanToArabic(result) != roman {
-
-		return 0, fmt.Errorf("некорректное римское число")
-
-	}
-
-	return result, nil
-
-}
-
-// В этой функции мы используем карту romanNumerals, которая содержит соответствия между римскими и арабскими числами.
-// Затем мы итерируем по символам в строке roman и складываем соответствующее значение из карты romanNumerals.
-// Если результат преобразования из арабского числа в римское не соответствует исходному римскому числу, то возвращаем ошибку.
-// TO DO.. Попровить ошибки..

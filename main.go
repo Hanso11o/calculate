0559820de5a49e577c8c17eef156899b7f3912df
denis erofeev <deniserofeev84@gmail.com>
2024-02-19 14:38:01 +0300

    fix

--- main.go
+++ main.go
@@ -8,23 +8,33 @@ import (
 )
 
 func main() {
-
-	fmt.Println("Введите выражение: ")
-
 	reader := bufio.NewReader(os.Stdin)
-	var args []string
-	// var operand string
 
+	fmt.Println("Введите выражение:")
+
+	var parts []string
 	for {
-		console, err := reader.ReadString('\n')
+		var input string
+		input, err := reader.ReadString('\n')
 		if err != nil {
-			panic(err)
+			fmt.Printf("ошибка чтения строки [%v]", err)
+			return
 
 		}
-		console = strings.TrimSpace(console)
 
-		args = strings.Split(console, " ")
+		parts = strings.Split(input, "-")
+		parts[0] = strings.Trim(parts[0], "\"")
+		parts[1] = strings.Trim(parts[1], "\"")
+		// parts[2] = strings.Trim(parts[2], "\"")
+		// if len(parts) != 3 {
+		// 	fmt.Print("не достаточно аргументов")
+		// 	return
+		// }
+
+		fmt.Println(len(parts))
+		fmt.Println(parts[0])
+		fmt.Println(parts[1])
+		// fmt.Println(parts[2])
 
 	}
-
 }

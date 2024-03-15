34396a8ea056d4cfe1497f05fb2eaad898e4a2e1
erofeev denis <deniserofeev84@gmail.com>
2024-03-15 14:23:14 +0300

    clear code action == '*'

--- main.go
+++ main.go
@@ -45,11 +45,6 @@ func main() {
 			panic("вторым аргументом выражения, подаваемым на вход, должна быть строка либо целое число")
 		}
 
-		if action == '*' || action == '/' {
-			strings.Contains(args[1], "\"")
-			panic("стоку можно умножать или делить только на строку")
-		}
-
 		if len(args[0]) < 1 || len(args[0]) > 12 && len(args[1]) < 1 || len(args[1]) > 12 {
 			panic("калькулятор может принимать строки длиной не более 10 символов")
 
@@ -78,24 +73,22 @@ func main() {
 		if action == '-' {
 			if strings.Contains(args[0], args[1]) {
 				args[0] = strings.Replace(args[0], args[1], "", 1)
+				fmt.Printf("\"%v\"\n", args[0])
 
 			} else {
-				fmt.Printf("\"%v\"", args[0])
+				fmt.Printf("\"%v\"\n", args[0])
 			}
 
 		}
 
 		if action == '*' {
-
-			arg0 := strings.ReplaceAll(args[0], " ", "")
-			arg1 := strings.ReplaceAll(args[1], " ", "")
-			mul, _ = strconv.Atoi(arg1)
+			mul, _ = strconv.Atoi(args[1])
 			if mul < 1 || mul > 10 {
 				panic("калькулятор может принимать на вход числа от 1 до 10 включительно")
 			}
 			result := ""
 			for i := 0; i < mul; i++ {
-				result += arg0
+				result += args[0]
 			}
 			fmt.Printf("\"%v\"\n", result)
 
